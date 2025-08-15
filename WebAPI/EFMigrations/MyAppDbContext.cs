cc9205f84237291bd427195300c86774f1ab1ffb
MANNY FLORY <mannyflory27@gmail.com>
2025-08-14 22:10:10 -0400

    set up entity framework and repository services for future development

---
+++ WebAPI/EFMigrations/MyAppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EFMigrations
+{
+    public class MyAppDbContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+
+        public MyAppDbContext(DbContextOptions<MyAppDbContext> options) : base(options) { }
+
+        // Optional: Override OnConfiguring if you want to configure the database connection directly here
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseSqlServer("YourConnectionString"); 
+        // }
+    }
+}
+
